649c02ad3dd68b7373bac47a4ff1b53c909b2adf
oleja_kosmolet <olegermolaev45@gmail.com>
2023-10-22 17:22:18 +0300

    Added task64 - output of the interval from n to 1

---
+++ task64/Program.cs
@@ -0,0 +1,11 @@
+﻿string PrintNumber(int N)
+{
+if (N == 1)
+    return "1";
+return $"{N} " + PrintNumber(N - 1);
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumber(N));
