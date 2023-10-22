16323dd25d932b5375d63f931b8528d4f57e431f
oleja_kosmolet <olegermolaev45@gmail.com>
2023-10-22 17:25:17 +0300

    Added task68 - Ackermann function

---
+++ task68/Program.cs
@@ -0,0 +1,16 @@
+﻿int A(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m != 0) && (n == 0))
+        return A(m - 1, 1);
+    else
+        return A(m - 1, A(m, n - 1));
+}
+
+Console.Write("Введите неотрицательное число 'm': ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число 'n': ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"A(m,n) = {A(m, n)}");
