76f808f41d2f826b18fdb2cf6f328e02e0aadb10
oleja_kosmolet <olegermolaev45@gmail.com>
2023-10-22 17:23:53 +0300

    Added task66 - sums of numbers in the interval from M to N

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿int PrintNumber(int M, int N)
+{
+    int sum = 0;
+    if (M == N)
+        return sum + N;
+    else
+        sum += M;
+    return  PrintNumber(M + 1, N) + sum;
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumber(M, N));
