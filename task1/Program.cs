ec4627bac774c02ef63466320a912be0f2c34b85
oleja_kosmolet <olegermolaev45@gmail.com>
2023-10-22 17:20:23 +0300

    Added task1 - draft

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// uint A(uint m, uint n)
+// {
+//     if (m == 0)
+//         return n + 1;
+//     else
+//       if ((m != 0) && (n == 0))
+//         return A(m - 1, 1);
+//     else
+//         return A(m - 1, A(m, n - 1));
+// }
+
+
+//          {n+1, если m = 0;
+// A(m,n) = {A(m - 1, 1), если m != 0, m = 0;
+//          {A(m - 1, A(m,n - 1)), если m > 0, m > 0. 
